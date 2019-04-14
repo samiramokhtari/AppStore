9b1a2c5d5190f7320ed309f7dab2e78d4073c70b
Samira Mokhtari <mokhtari.9310@yahoo.com>
2019-04-14 19:58:38 +0330

    اضافه کردن کنترلر کامنت

---
+++ AppStore.UI/Controllers/CommentDataController.cs
@@ -0,0 +1,20 @@
+﻿using AppStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace AppStore.UI.Controllers
+{
+    public class CommentDataController : ApiController
+    {
+
+        // GET api/<controller>
+        public IEnumerable<Comment> Get(int Productid)
+        {
+            return new Biz.CommentBiz().Get(Productid);
+        }
+    }
+}
